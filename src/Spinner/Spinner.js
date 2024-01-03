32cb635e0596bfd3d6431530a263b5b618e0114b
William Castellano <williamc246@gmail.com>
2024-01-02 20:02:36 -0800

    initial commit

--- src/Spinner/Spinner.js
+++ src/Spinner/Spinner.js
@@ -5,9 +5,71 @@ const logoSrc =
     "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png";
 
 function Spinner() {
-    let message = "Click me!!";
-    const [value, setValue] = React.useState("Click me!");
-    const [num, setNum] = React.useState(1);
+    const MODE_STOPPED = 0;
+    const MODE_SLOW = 1;
+    const MODE_MEDIUM = 2;
+    const MODE_FAST = 3;
+    const MODE_VERY_FAST = 4;
+    const DIRECTION_LEFT = 0;
+    const DIRECTION_RIGHT = 1;
+    const [value, setValue] = React.useState("Click to Start");
+    const [textField, setTextField] = React.useState("");
+    const [spinnerMode, setSpinnerMode] = React.useState(MODE_STOPPED);
+    const [direction, setDirection] = React.useState(DIRECTION_RIGHT);
+
+    const advanceState = () => {
+        let newVal = spinnerMode;
+        newVal = direction === DIRECTION_RIGHT ? newVal + 1 : newVal - 1;
+        if (newVal > 4) {
+            setDirection(DIRECTION_LEFT);
+            newVal = 3;
+        }
+        if (newVal < 0) {
+            setDirection(DIRECTION_RIGHT);
+            newVal = 1;
+        }
+        console.log('*** new spinner mode: ', newVal);
+        setSpinnerMode(newVal);
+    }
+
+    const updateAnimation = () => {
+        let directText = '';
+        let msg = '';
+        let duration = 0;
+        switch (spinnerMode) {
+            case MODE_STOPPED:
+                directText = direction === DIRECTION_RIGHT ? '>' : '<';
+                msg = "Very Slow";
+                duration = 5.5;
+                break;
+            case MODE_SLOW:
+                directText = direction === DIRECTION_RIGHT ? '>>' : '<<';
+                msg = "Slow";
+                duration = 2;
+                break
+            case MODE_MEDIUM:
+                directText = direction === DIRECTION_RIGHT ? '>>>' : '<<<';
+                msg = "Medium";
+                duration = 0.75;
+                break
+            case MODE_FAST:
+                directText = direction === DIRECTION_RIGHT ? '>>>>' : '<<<<';
+                msg = "Fast";
+                duration = 0.4;
+                break;
+            case MODE_VERY_FAST:
+                directText = direction === DIRECTION_RIGHT ? '>>>>>' : '<<<<<';
+                msg = "Very Fast";
+                duration = 0.3;
+                break;
+            default:
+                duration = 0;
+                break;
+        }
+        setValue(directText);
+        setTextField(msg);
+        return duration;
+    }
 
     const onClick = () => {
         var element = document.getElementById("colorButton");
@@ -15,25 +77,25 @@ function Spinner() {
             element.style.backgroundColor === "grey" ? "white" : "grey";
         element.className = "color-button-expanded";
         var appLogo = document.getElementById("appLogo");
-        appLogo.className =
-            appLogo.className === "App-logo" ? "App-logo-reverse" : "App-logo";
-        setNum(num + 1);
-        setValue("You got clicked: " + num);
-        appLogo.style.animationDuration = "0.5s";
+        appLogo.className = direction === DIRECTION_RIGHT ? 'App-logo' : 'App-logo-reverse';
+        advanceState();
+        const duration = updateAnimation() + "s";
+        appLogo.style.animationDuration = duration;
     };
     return (
         <div className="Spinner">
             <header className="Spinner-header">
-                <button id="colorButton" class="color-button" onClick={onClick}>
+                <button id="colorButton" className="color-button" onClick={onClick}>
                     <img
                         src={logoSrc}
                         id="appLogo"
                         className="App-logo"
                         alt="logo"
                     />
-                    <p>
-                        <code>{value}</code>
-                    </p>
+                    <div className="info-text">
+                        <div><code>{value}</code></div>
+                        <div>{textField}</div>
+                    </div>
                 </button>
             </header>
         </div>
